be186e6d483e9627a951dd27995d8aeba3fddfed
Vijilc <vijil.cheruvalath@janaagraha.org>
2024-05-16 18:38:45 +0530

    Styles added

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -2,7 +2,7 @@ export const environment = {
   api: {
     url: 'http://localhost:8080/api/v1/',
   },
-  environment: 'local 123123',
+  environment: 'local',
   isProduction: false,
   versionCheckURL: window.location.origin + '/version.json',
   STORAGE_BASEURL: 'https://jana-cityfinance-stg.s3.ap-south-1.amazonaws.com',
