e36aae7bb3025bc7fd43d33454c36b11338c2203
Vijilc <vijil.cheruvalath@janaagraha.org>
2024-05-16 16:14:43 +0530

    Common components created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { MenuComponent } from './shared/components/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [CommonModule,
+    RouterOutlet,
+    HttpClientModule,
+    FooterComponent,
+    MenuComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { MaintenanceGuard } from './core/guards/maintenance/maintenance.guard';
+import { MaintenanceComponent } from './features/maintenance/maintenance.component';
+import { ErrorComponent } from './features/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        canActivate: [MaintenanceGuard],
+    },
+    {
+        path: 'maintenance',
+        component: MaintenanceComponent,
+    },
+    {
+        path: 'error',
+        component: ErrorComponent,
+        canActivate: [MaintenanceGuard],
+    },
+    {
+        path: '**',
+        redirectTo: 'error',
+    },
+];
