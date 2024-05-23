e9c01e68dfd3992b5c17f1fda1d00fb6910823c0
Kushal-MP08 <kushal.mp@janaagraha.org>
2024-05-23 17:31:05 +0530

    Added base href

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -13,7 +13,7 @@ import { APP_BASE_HREF } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    {provide:APP_BASE_HREF, useValue: '/xvi-fc/'},
+    // {provide:APP_BASE_HREF, useValue: '/xvi-fc/'},
     provideRouter(routes), provideAnimationsAsync(),
     importProvidersFrom(HttpClientModule),
 
