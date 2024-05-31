338a69040496c4d93201fe2056c0e1e2a60005bc
jeevad <jeevananthamcse@gmail.com>
2024-05-31 16:50:16 +0530

    New component created for xvi form

---
+++ src/app/features/xvi-fc-form/xvi-fc-form.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { XviFcFormComponent } from './xvi-fc-form.component';
+
+describe('XviFcFormComponent', () => {
+  let component: XviFcFormComponent;
+  let fixture: ComponentFixture<XviFcFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [XviFcFormComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(XviFcFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
