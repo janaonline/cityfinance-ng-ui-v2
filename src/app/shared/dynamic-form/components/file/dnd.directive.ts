dd9cc158255fd54496a133d829a23653c2a989bd
jeevad <jeevananthamcse@gmail.com>
2024-06-03 17:55:43 +0530

    Event type added

--- src/app/shared/dynamic-form/components/file/dnd.directive.ts
+++ src/app/shared/dynamic-form/components/file/dnd.directive.ts
@@ -15,26 +15,26 @@ export class DndDirective {
     @Output() fileDropped = new EventEmitter<any>();
 
     // Dragover listener
-    @HostListener('dragover', ['$event']) onDragOver(evt: any) {
+    @HostListener('dragover', ['$event']) onDragOver(evt: DragEvent) {
         evt.preventDefault();
         evt.stopPropagation();
         this.fileOver = true;
     }
 
     // Dragleave listener
-    @HostListener('dragleave', ['$event']) public onDragLeave(evt: any) {
+    @HostListener('dragleave', ['$event']) public onDragLeave(evt: DragEvent) {
         evt.preventDefault();
         evt.stopPropagation();
         this.fileOver = false;
     }
 
     // Drop listener
-    @HostListener('drop', ['$event']) public ondrop(evt: any) {
+    @HostListener('drop', ['$event']) public ondrop(evt: DragEvent) {
         evt.preventDefault();
         evt.stopPropagation();
         this.fileOver = false;
-        let files = evt.dataTransfer.files;
-        if (files.length > 0) {
+        let files = evt.dataTransfer?.files;
+        if (files && files.length > 0) {
             this.fileDropped.emit(files);
         }
     }
