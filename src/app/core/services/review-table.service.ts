a1cc5a529ec5c084eecb6f7eb58d76f35712b78a
Vijilc <vijil.cheruvalath@janaagraha.org>
2024-06-20 20:09:24 +0530

    Review table integration

--- src/app/core/services/review-table.service.ts
+++ src/app/core/services/review-table.service.ts
@@ -9,13 +9,13 @@ export class ReviewTableService {
 
   constructor(private http: HttpClient) { }
 
-  getFormList(state: string) {
-    const params = { state };
+  getFormList(paginationParams:any) {
+    const params = { state, skip: 0, limit: 10 };
 
     // {{url}}xviFc/form_list/?state=5dcf9d7416a06aed41c748f0&skip=0&limit=10
 
     return this.http.post(
-      `${environment.api.url}xviFc/form_list/?state=5dcf9d7416a06aed41c748f0`, { params }
+      `${environment.api.url}xviFc/form_list?state=${state}&skip=${paginationParams.skip}&limit=${paginationParams.page}`, { params }
     );
   }
 }
