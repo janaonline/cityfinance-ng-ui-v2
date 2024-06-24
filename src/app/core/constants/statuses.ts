28241b61b8d134715baa3ab59fb9b8331c09d38b
jeevad <jeevananthamcse@gmail.com>
2024-06-24 18:17:37 +0530

    Issue fixed

--- src/app/core/constants/statuses.ts
+++ src/app/core/constants/statuses.ts
@@ -1,11 +1,11 @@
 export const FORM_STATUSES: any = {
     IN_PROGRESS: { key: 'IN_PROGRESS', label: 'IN PROGRESS', class: 'alert-warning' },
     NOT_STARTED: { key: 'NOT_STARTED', label: 'NOT STARTED', class: 'alert-secondary' },
-    UNDER_REVIEW_BY_STATE: { key: 'UNDER_REVIEW_BY_STATE', label: 'UNDER_REVIEW_BY_STATE', class: 'alert-info' },
-    RETURNED_BY_STATE: { key: 'RETURNED_BY_STATE', label: 'RETURNED_BY_STATE', class: 'alert-danger' },
-    UNDER_REVIEW_BY_XVIFC: { key: 'UNDER_REVIEW_BY_XVIFC', label: 'UNDER_REVIEW_BY_XVIFC', class: 'alert-info' },
-    SUO_MOTO_STATE: { key: 'SUO_MOTO_STATE', label: 'SUO_MOTO_STATE', class: 'alert-success' },
-    RETURNED_BY_XVIFC: { key: 'RETURNED_BY_XVIFC', label: 'RETURNED_BY_XVIFC', class: 'alert-danger' },
-    APPROVED_BY_XVIFC: { key: 'APPROVED_BY_XVIFC', label: 'APPROVED_BY_XVIFC', class: 'alert-success' },
-    SUO_MOTO_XVIFC: { key: 'SUO_MOTO_XVIFC', label: 'SUO_MOTO_XVIFC', class: 'alert-success' },
+    UNDER_REVIEW_BY_STATE: { key: 'UNDER_REVIEW_BY_STATE', label: 'UNDER REVIEW BY STATE', class: 'alert-info' },
+    RETURNED_BY_STATE: { key: 'RETURNED_BY_STATE', label: 'RETURNED BY STATE', class: 'alert-danger' },
+    UNDER_REVIEW_BY_XVIFC: { key: 'UNDER_REVIEW_BY_XVIFC', label: 'UNDER REVIEW BY XVIFC', class: 'alert-info' },
+    SUO_MOTO_STATE: { key: 'SUO_MOTO_STATE', label: 'SUO MOTO STATE', class: 'alert-success' },
+    RETURNED_BY_XVIFC: { key: 'RETURNED_BY_XVIFC', label: 'RETURNED BY XVIFC', class: 'alert-danger' },
+    APPROVED_BY_XVIFC: { key: 'APPROVED_BY_XVIFC', label: 'APPROVED BY XVIFC', class: 'alert-success' },
+    SUO_MOTO_XVIFC: { key: 'SUO_MOTO_XVIFC', label: 'SUO MOTO XVIFC', class: 'alert-success' },
 }
