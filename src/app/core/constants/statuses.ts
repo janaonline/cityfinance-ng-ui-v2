7c53f29e021d657c28255c10ea6bcc2b1079918c
Vijilc <vijil.cheruvalath@janaagraha.org>
2024-06-27 18:16:06 +0530

    Status removed and additional changes added

--- src/app/core/constants/statuses.ts
+++ src/app/core/constants/statuses.ts
@@ -4,8 +4,6 @@ export const FORM_STATUSES: any = {
     UNDER_REVIEW_BY_STATE: { key: 'UNDER_REVIEW_BY_STATE', label: 'UNDER REVIEW BY STATE', class: 'alert-info' },
     RETURNED_BY_STATE: { key: 'RETURNED_BY_STATE', label: 'RETURNED BY STATE', class: 'alert-danger' },
     UNDER_REVIEW_BY_XVIFC: { key: 'UNDER_REVIEW_BY_XVIFC', label: 'UNDER REVIEW BY XVIFC', class: 'alert-info' },
-    SUO_MOTO_STATE: { key: 'SUO_MOTO_STATE', label: 'SUO MOTO STATE', class: 'alert-success' },
     RETURNED_BY_XVIFC: { key: 'RETURNED_BY_XVIFC', label: 'RETURNED BY XVIFC', class: 'alert-danger' },
     APPROVED_BY_XVIFC: { key: 'APPROVED_BY_XVIFC', label: 'APPROVED BY XVIFC', class: 'alert-success' },
-    SUO_MOTO_XVIFC: { key: 'SUO_MOTO_XVIFC', label: 'SUO MOTO XVIFC', class: 'alert-success' },
 }
