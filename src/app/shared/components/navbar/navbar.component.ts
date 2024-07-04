64ab9620b30cecaa6ee89e53d07ad7628c7a3f02
jeevad <jeevananthamcse@gmail.com>
2024-07-04 18:54:29 +0530

    Dynamic file upload created

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -86,9 +86,9 @@ export class NavbarComponent {
     }
     const role = this.user ? this.user.role : '';
     this.menus = [
-      ...this.menus,
+      // ...this.menus,
       (role === USER_TYPE.PMU && { name: 'State resources', href: '/mohua-form/state-resource-manager' }),
-      (this.notInRole([USER_TYPE.PMU, USER_TYPE.XVIFC_STATE]) && { name: '15<sup>th</sup> FC Grants', href: '/fc-home-page' }),
+      // (this.notInRole([USER_TYPE.PMU, USER_TYPE.XVIFC_STATE]) && { name: '15<sup>th</sup> FC Grants', href: '/fc-home-page' }),
       (role === USER_TYPE.ULB && { name: `XVI FC Data Collection`, link: '/xvifc-form' }),
       (role === USER_TYPE.XVIFC_STATE && { name: `Review XVI FC`, link: '/admin/xvi-fc-review' }),
       (this.notInRole([USER_TYPE.ULB, USER_TYPE.XVIFC_STATE]) && { name: `Rankings'22 Dashboard`, href: '/rankings/review-rankings-ulbform' }),
