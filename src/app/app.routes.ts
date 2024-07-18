5083f8bc43c6db130b9cc2054359fc66d790744d
Vijilc <vijil.cheruvalath@janaagraha.org>
2024-07-18 10:28:11 +0530

    Pdf generator files removed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { MaintenanceGuard } from './core/guards/maintenance/maintenance.guard';
 import { MaintenanceComponent } from './features/maintenance/maintenance.component';
 import { ErrorComponent } from './features/error/error.component';
 import { authGuard } from './core/guards/auth.guard';
-import { PdfContentComponent } from './pdf-content/pdf-content.component';
 
 export const routes: Routes = [
     {
@@ -38,10 +37,6 @@ export const routes: Routes = [
     {
         path: 'maintenance',
         component: MaintenanceComponent,
-    },
-    {
-        path: 'pdf',
-        component: PdfContentComponent,
     },
     {
         path: 'error',
