a73f52c3ac6167834f1a0cac5e8b447cd9eda376
mdshamoon <mohdshamoon841@gmail.com>
2024-08-08 19:23:36 +0530

    written test cases and added ci pipeline

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,7 @@ module.exports = function (config) {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/project-name'),
+      dir: require('path').join(__dirname, './coverage'),
       subdir: '.',
       reporters: [{ type: 'html' }, { type: 'text-summary' }, { type: 'lcov' }],
     },
