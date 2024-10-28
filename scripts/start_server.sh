eeccf8d97c69a778e9ee41d1bbecfd1c12906c38
mdshamoon <mohdshamoon841@gmail.com>
2024-10-28 16:12:26 +0530

    added first chart

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-export PATH="$PATH:/usr/local/nvm/versions/node/v20.17.0/bin/"
 cd /var/www/html/cityfinance/cityfinance-ng-ui-v2
-npm i
-npm run build1
 rm -rf dist_live/
 mv dist/ dist_live/
