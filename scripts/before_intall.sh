102e0b72bd4419407669e00a3dc899112683fc70
mdshamoon <mohdshamoon841@gmail.com>
2024-10-29 23:37:27 +0530

    added more to deploy

--- scripts/before_intall.sh
+++ scripts/before_intall.sh
@@ -2,10 +2,4 @@
 # Before install
 #!/bin/bash
 
-rm -rf /var/www/html/cityfinance/cityfinancev2/
-mkdir -p /var/www/html/cityfinance/cityfinancev2/
 
-mv /var/www/html/cityfinance/cityfinancev2-deploy/.* /var/www/html/cityfinance/cityfinancev2/ 2>/dev/null
-mv /var/www/html/cityfinance/cityfinancev2-deploy/* /var/www/html/cityfinance/cityfinancev2/
-
-rm -rf /var/www/html/cityfinance/cityfinancev2-deploy/
