e29c172e26b15af73fe6ceb5172fb944a348f519
mdshamoon <mohdshamoon841@gmail.com>
2024-10-29 22:24:10 +0530

    fixed deploy

--- scripts/before_intall.sh
+++ scripts/before_intall.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
-# Before install
 
-if [ -d /var/www/html/cityfinance/cityfinancev2 ]; then
-  echo "Deleting /var/www/html/cityfinance/cityfinancev2..."
-  rm -rf /var/www/html/cityfinance/cityfinancev2
-fi
+# Before install
+#!/bin/bash
+
+rm -rf /var/www/html/cityfinance/cityfinancev2/
+mkdir -p /var/www/html/cityfinance/cityfinancev2/
+
+mv /var/www/html/cityfinance/cityfinancev2-deploy/.* /var/www/html/cityfinance/cityfinancev2/ 2>/dev/null
+mv /var/www/html/cityfinance/cityfinancev2-deploy/* /var/www/html/cityfinance/cityfinancev2/
+
+rm -rf /var/www/html/cityfinance/cityfinancev2-deploy/
