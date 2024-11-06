54f6a4738d7770f619cffafd78c63995b44ee11f
mdshamoon <mohdshamoon841@gmail.com>
2024-11-06 13:00:01 +0530

    added all dbt models

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
 
-rm -rf /var/www/html/cityfinance/cityfinancev2/
-mkdir -p /var/www/html/cityfinance/cityfinancev2/
-
-mv /var/www/html/cityfinance/cityfinancev2-deploy/.* /var/www/html/cityfinance/cityfinancev2/ 2>/dev/null
-mv /var/www/html/cityfinance/cityfinancev2-deploy/* /var/www/html/cityfinance/cityfinancev2/
-
-rm -rf /var/www/html/cityfinance/cityfinancev2-deploy/
