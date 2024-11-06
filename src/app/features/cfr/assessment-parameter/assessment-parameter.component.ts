9bd40f2cbef06e024923b452df7d85f4671c97ae
Navinder Singh <146955910+navinder-dev@users.noreply.github.com>
2024-11-06 16:40:36 +0530

    Remove commented code | add loader

--- src/app/features/cfr/assessment-parameter/assessment-parameter.component.ts
+++ src/app/features/cfr/assessment-parameter/assessment-parameter.component.ts
@@ -1,8 +1,9 @@
-import { CommonModule, ViewportScroller } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import Swal from 'sweetalert2';
+import { LoaderComponent } from '../../../shared/components/loader/loader.component';
 import { BreadcrumbComponent, BreadcrumbLink } from '../breadcrumb/breadcrumb.component';
 import { MatCommonTableComponent } from '../mat-common-table/mat-common-table.component';
 import { FiscalRankingService } from '../services/fiscal-ranking.service';
@@ -12,11 +13,16 @@ import { FiscalRankingService } from '../services/fiscal-ranking.service';
   templateUrl: './assessment-parameter.component.html',
   styleUrls: ['./assessment-parameter.component.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, BreadcrumbComponent, MatCommonTableComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    BreadcrumbComponent,
+    MatCommonTableComponent,
+    LoaderComponent,
+  ],
 })
 export class AssessmentParameterComponent implements OnInit {
   constructor(
-    private viewportScroller: ViewportScroller,
     private route: ActivatedRoute,
     private router: Router,
     private fiscalRankingService: FiscalRankingService,
@@ -26,7 +32,7 @@ export class AssessmentParameterComponent implements OnInit {
     if (id) this.pageKey = id;
   }
   pageKey: string = 'resourceMobilization';
-  isLoadingResults: boolean = false;
+  isLoadingResults: boolean = true;
   apiResponse: any = {};
   parameterData: any = {};
   assestParameters = {
@@ -60,28 +66,11 @@ export class AssessmentParameterComponent implements OnInit {
       class: 'disabled',
     },
   ];
-  // allPageData: object | any = {};
-
-  // currentPageData: object | any = {};
-  // routerSubs: any;
-  // isApiInProgress: boolean = true;
 
   ngOnInit(): void {
-    this.viewportScroller.scrollToPosition([0, 0]); // Scroll to the top
     this.getPageData();
-    //   this.currentPageData = this.allPageData[this.pageKey];
   }
-  // checkRouterForApi() {
-  //   this.isLoadingResults = true;
-  //   this.routerSubs = this.router.events.subscribe((event) => {
-  //     console.log('---->', event);
-  //     if (event instanceof NavigationEnd) {
-  //       const urlArray = event.url.split('/');
-  //       this.pageKey = urlArray[3];
-  //       this.getPageData();
-  //     }
-  //   });
-  // }
+
   setPageKey(event: Event): void {
     const selectElement = event.target as HTMLSelectElement;
     const selectedValue = selectElement.value;
@@ -96,7 +85,6 @@ export class AssessmentParameterComponent implements OnInit {
 
   updateBreadCrumb(parameter: string) {
     const currentPageLink = {
-      // label: `Ranking assessment parameter : ${this.currentPageData?.name}`,
       label: `Ranking assessment parameter : ${parameter}`,
       url: '/cfr/participated-states-ut',
       class: 'disabled',
@@ -104,36 +92,21 @@ export class AssessmentParameterComponent implements OnInit {
     this.breadcrumbLinks.splice(1, 1, currentPageLink);
   }
 
-  // setPageKey(data: any) {
-  //   // this.currentPageData = this.allPageData[data];
-  //   this.router.navigateByUrl(`cfr/assesst-parameters/${data}`)
-  // }
   getPageData() {
+    this.isLoadingResults = true;
     this.fiscalRankingService.callGetMethod(`scoring-fr/assessment-parameters`, null).subscribe({
       next: (res: any) => {
         // console.log(res);
         this.parameterData = res?.data?.[this.pageKey];
         this.apiResponse = res?.data;
-        // this.allPageData = res?.data;
-        // this.currentPageData = this.allPageData[this.pageKey];
         this.updateBreadCrumb(this.parameterData?.name);
         this.isLoadingResults = false;
       },
       error: (error) => {
         Swal.fire('Error', error?.message ?? 'Something went wrong', 'error');
-        // this.currentPageData = {};
         this.parameterData = {};
         this.isLoadingResults = false;
       },
     });
   }
-
-  // ngOnDestroy() {
-  //   this.isLoadingResults = false;
-  //   // this.routerSubs.unsubscribe();
-  // }
-
-  // isEmptyObject(obj: any): boolean {
-  //   return obj && Object.keys(obj).length === 0;
-  // }
 }
--- src/app/features/cfr/ulb-details/comparison/comparison.component.ts
+++ src/app/features/cfr/ulb-details/comparison/comparison.component.ts
@@ -1,21 +1,19 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Chart, registerables } from 'chart.js';
-// import { getPopulationCategory } from 'src/app/util/common';
-// import { FiscalRankingService, UlbData } from '../../fiscal-ranking.service';
-import { ComparisionFiltersComponent } from '../comparision-filters/comparision-filters.component';
-import { MaterialModule } from '../../../../material.module';
 import { getPopulationCategory } from '../../../../core/util/common';
+import { MaterialModule } from '../../../../material.module';
+import { PreLoaderComponent } from '../../../../shared/components/pre-loader/pre-loader.component';
 import { FiscalRankingService, UlbData } from '../../services/fiscal-ranking.service';
-Chart.register(...registerables)
-
+import { ComparisionFiltersComponent } from '../comparision-filters/comparision-filters.component';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-comparison',
   templateUrl: './comparison.component.html',
   styleUrls: ['./comparison.component.scss'],
   standalone: true,
-  imports: [MaterialModule],
+  imports: [MaterialModule, PreLoaderComponent],
 })
 export class ComparisonComponent implements OnChanges {
   public chart: any;
@@ -26,15 +24,15 @@ export class ComparisonComponent implements OnChanges {
     { id: 'overAll', label: 'Over All', maxScore: 1200 },
     { id: 'resourceMobilization', label: 'Resource mobilization', maxScore: 600 },
     { id: 'expenditurePerformance', label: 'Expenditure performance', maxScore: 300 },
-    { id: 'fiscalGovernance', label: 'Fiscal Governance', maxScore: 300 }
+    { id: 'fiscalGovernance', label: 'Fiscal Governance', maxScore: 300 },
   ];
   type = 'overAll';
-
+  isPreLoader: boolean = true;
   datasetsFilter: any = {
     // "State Average": true,
     // "National Average": true,
-    "Population Average": true,
-  }
+    'Population Average': true,
+  };
 
   ulbs: any = [];
   hasNonBucketUlb = false;
@@ -42,8 +40,8 @@ export class ComparisonComponent implements OnChanges {
 
   constructor(
     private matDialog: MatDialog,
-    private fiscalRankingService: FiscalRankingService
-  ) { }
+    private fiscalRankingService: FiscalRankingService,
+  ) {}
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['ulb'].currentValue && this.ulbs.length == 0) {
@@ -54,11 +52,15 @@ export class ComparisonComponent implements OnChanges {
   }
 
   getBarchartData() {
-    const ulbQuery = this.ulbs.map((item: { ulb: any; }) => `ulb[]=${item?.ulb}`).join('&');
-    this.fiscalRankingService.getBarchartData(ulbQuery, this.ulb?.populationBucket).subscribe((res: any) => {
-      this.allTypeGraphData = res.graphData;
-      this.createChart();
-    })
+    this.isPreLoader = true;
+    const ulbQuery = this.ulbs.map((item: { ulb: any }) => `ulb[]=${item?.ulb}`).join('&');
+    this.fiscalRankingService
+      .getBarchartData(ulbQuery, this.ulb?.populationBucket)
+      .subscribe((res: any) => {
+        this.allTypeGraphData = res.graphData;
+        this.createChart();
+      });
+    this.isPreLoader = false;
   }
 
   get graphData() {
@@ -66,69 +68,61 @@ export class ComparisonComponent implements OnChanges {
   }
 
   get suggestedMinMax() {
-    return this.types?.find(type => type.id == this.type)?.maxScore;
+    return this.types?.find((type) => type.id == this.type)?.maxScore;
   }
-
-  // getBarBackgroundColor(item: any) {
-  //   // console.log("item", item)
-  //   if (item?.type != 'bar') return item?.backgroundColor;
-  //   const colors = this.ulbs.map((ulb: { populationBucket: any; }) => {
-  //     return ulb?.populationBucket == this.ulb?.populationBucket ? item?.backgroundColor : 'red'
-  //   });
-  //   // console.log("colors--->", colors);
-  //   return colors;
-  // }
 
   createChart() {
     if (this.chart) this.chart.destroy();
-    // console.log('ulb', this.ulb);
-    // console.log('ulbs', this.ulbs);
-    this.hasNonBucketUlb = this.ulbs.some((ulb: { populationBucket: any; }) => ulb?.populationBucket != this.ulb?.populationBucket);
+    this.hasNonBucketUlb = this.ulbs.some(
+      (ulb: { populationBucket: any }) => ulb?.populationBucket != this.ulb?.populationBucket,
+    );
     const that = this;
-    this.chart = new Chart("bar-chart-with-line", {
+    this.chart = new Chart('bar-chart-with-line', {
       type: 'bar',
       data: {
         ...this.graphData,
         datasets: this.graphData.datasets.map((item: any) => ({
           ...item,
           // backgroundColor: this.getBarBackgroundColor(item),
-          hidden: !(this.datasetsFilter[item.label] != undefined ? this.datasetsFilter[item.label] : true)
+          hidden: !(this.datasetsFilter[item.label] != undefined
+            ? this.datasetsFilter[item.label]
+            : true),
         })),
       },
       options: {
         responsive: true,
         maintainAspectRatio: false,
         layout: {
-          padding: 0
+          padding: 0,
         },
         plugins: {
           legend: {
             labels: {
               font: {
                 size: 12,
-                family: 'Montserrat'
-              }
-            }
+                family: 'Montserrat',
+              },
+            },
           },
           tooltip: {
             titleFont: {
               size: 14,
-              family: 'Montserrat'
+              family: 'Montserrat',
             },
             bodyFont: {
               size: 12,
-              family: 'Montserrat'
-            }
-          }
+              family: 'Montserrat',
+            },
+          },
         },
         scales: {
           x: {
             ticks: {
               font: {
                 size: 12,
-                family: 'Montserrat'
-              }
-            }
+                family: 'Montserrat',
+              },
+            },
           },
           y: {
             ticks: {
@@ -138,66 +132,36 @@ export class ComparisonComponent implements OnChanges {
               // stepSize: 100,
               font: {
                 size: 12,
-                family: 'Montserrat'
-              }
-            }
+                family: 'Montserrat',
+              },
+            },
           },
-        }
-      }
-      // options: {
-      //   maintainAspectRatio: false,
-      //   scales: {
-      //     yAxes: [{
-      //       ticks: {
-      //         suggestedMin: this.suggestedMinMax,
-      //         suggestedMax: this.suggestedMinMax,
-      //         beginAtZero: true,
-      //         stepSize: 100
-      //       }
-      //     }],
-      //     xAxes: [{
-      //       gridLines: {
-      //         display: false
-      //       }
-      //     }],
-      //   },
-      //   legend: {
-      //     position: 'bottom',
-      //     display: true,
-      //     labels: {
-      //       boxWidth: 10
-      //     },
-      //     // onClick: function (event: any, legendItem: any) {
-      //     //   if (Object.keys(that.datasetsFilter).includes(legendItem.text)) {
-      //     //     that.datasetsFilter[legendItem.text] = legendItem?.hidden;
-      //     //   }
-      //     //   Chart.defaults.plugins.legend.onClick.call(this, event, legendItem, );
-      //     // }
-      //   }
-      // }
+        },
+      },
     } as any);
-
-    // this.chart.canvas.style.height = '55vh';
   }
 
   openFilter() {
-    this.matDialog.open(ComparisionFiltersComponent, {
-      width: '500px',
-      data: {
-        ulbs: this.ulbs,
-        ulb: this.ulb,
-        bucketShortName: this.bucketShortName,
-        datasetsFilter: this.datasetsFilter
-      }
-    }).afterClosed().subscribe(res => {
-      if (res) {
-        if (res == 'reset') return this.reset();
-        // console.log("res", res);
-        this.ulbs = res.ulbs;
-        this.datasetsFilter = res.datasetsFilter;
-        this.getBarchartData();
-      }
-    });
+    this.matDialog
+      .open(ComparisionFiltersComponent, {
+        width: '500px',
+        data: {
+          ulbs: this.ulbs,
+          ulb: this.ulb,
+          bucketShortName: this.bucketShortName,
+          datasetsFilter: this.datasetsFilter,
+        },
+      })
+      .afterClosed()
+      .subscribe((res) => {
+        if (res) {
+          if (res == 'reset') return this.reset();
+          // console.log("res", res);
+          this.ulbs = res.ulbs;
+          this.datasetsFilter = res.datasetsFilter;
+          this.getBarchartData();
+        }
+      });
   }
 
   reset() {
@@ -206,63 +170,8 @@ export class ComparisonComponent implements OnChanges {
     this.datasetsFilter = {
       // "State Average": true,
       // "National Average": true,
-      "Population Average": true,
+      'Population Average': true,
     };
     this.getBarchartData();
   }
-
-  // chartdata: any[] = []
-  // labeldata: number[] = [];
-  // realdata: number[] = [];
-  // colordata: string[] = [];
-
-  // loadchartdata() {
-  //   this.fiscalRankingService.loadsalesdata().subscribe(item => {
-  //     this.chartdata = item;
-  //     if (this.chartdata != null) {
-  //       this.chartdata.map(o => {
-  //         this.labeldata.push(o.year);
-  //         this.realdata.push(o.amount);
-  //         this.colordata.push(o.colorcode)
-  //       })
-  //       this.Renderbarchart(this.labeldata, this.realdata, this.colordata);
-  //       // this.Renderpiechart(this.labeldata, this.realdata, this.colordata);
-  //       // this.Renderdoughnutchart(this.labeldata, this.realdata, this.colordata);
-  //       // this.RenderPAchart(this.labeldata, this.realdata, this.colordata);
-  //       // this.RenderRadarchart(this.labeldata, this.realdata, this.colordata);
-  //       // this.Renderlinechart(this.labeldata, this.realdata, this.colordata);
-  //       // this.RenderBubblechart();
-  //       // this.RenderScatterchart();
-  //     }
-  //   });
-  // }
-
-  // Renderbarchart(labeldata: any, valuedata: any, colordata: any) {
-  //   this.Renderchart(labeldata, valuedata, colordata, 'barchart', 'bar')
-  // }
-
-  // Renderchart(labeldata: any, valuedata: any, colordata: any, chartid: string, charttype: any) {
-  //   const mychar = new Chart(chartid, {
-  //     type: charttype,
-  //     data: {
-  //       labels: labeldata,
-  //       datasets: [
-  //         {
-  //           label: 'Yearly sales',
-  //           data: valuedata,
-  //           backgroundColor: colordata,
-
-  //         }
-  //       ]
-  //     },
-  //     options: {
-  //       scales: {
-  //         y: {
-  //           beginAtZero: false
-  //         }
-  //       }
-  //     }
-
-  //   });
-  // }
 }
