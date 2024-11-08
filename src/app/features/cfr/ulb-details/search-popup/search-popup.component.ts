e73c035cbe131c55fbac664ffd81af6ef48f51c4
jeevad <jeevananthamcse@gmail.com>
2024-11-08 13:16:04 +0530

    Search ulb autocomplete added

--- src/app/features/cfr/ulb-details/search-popup/search-popup.component.ts
+++ src/app/features/cfr/ulb-details/search-popup/search-popup.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { RouterModule } from '@angular/router';
+import { debounceTime, distinctUntilChanged, map, of, switchMap, tap } from 'rxjs';
 import { MaterialModule } from '../../../../material.module';
 import { FiscalRankingService } from '../../services/fiscal-ranking.service';
-import { RouterModule } from '@angular/router';
-// import { FiscalRankingService } from '../../fiscal-ranking.service';
 
 @Component({
   selector: 'app-search-popup',
@@ -12,23 +13,88 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [MaterialModule, RouterModule],
 })
-export class SearchPopupComponent {
+export class SearchPopupComponent implements OnInit {
 
-  ulbs: any = [];
+  // ulbs: any = [];
   query: string = '';
+
+  searchField = new FormControl();
+  searchResults: any = [];
+  ulbs: any = [];
+  datasetsFilter: any = {};
+  isSearching: boolean = false;
+  noDataFound: boolean = false;
+  showSearches: boolean = false;
+  filteredOptions: any = [];
 
   constructor(
     private matDialog: MatDialog,
     private fiscalRankingService: FiscalRankingService
   ) { }
 
-  search() {
-    this.fiscalRankingService.searchUlb(this.query).subscribe((res: any) => {
-      this.ulbs = res?.ulbs;
-    })
+  ngOnInit() {
+    this.onSearchValueChange();
   }
+
   close() {
     this.matDialog.closeAll();
+  }
+
+  onSearchValueChange() {
+    const search$ = this.searchField.valueChanges.pipe(
+      map((value: any) => {
+        return value;
+      }),
+      debounceTime(500),
+      distinctUntilChanged(),
+      tap(() => (this.isSearching = true)),
+      switchMap((term) => (term ? this.fiscalRankingService.searchUlb(term) : of<any>({ data: this.filteredOptions }))),
+      tap(() => {
+        (this.isSearching = false), (this.showSearches = true);
+      }),
+    );
+
+    search$.subscribe((resp) => {
+      this.isSearching = false;
+      if (resp['ulbs'].length > 0) {
+        this.noDataFound = false;
+      } else {
+        this.noDataFound = true;
+      }
+      // this.ulb.name
+      // console.log("resp", resp["ulbs"])
+      this.filteredOptions = resp['ulbs'];
+    });
+  }
+
+  async addUlb(ulb: any) {
+    // if (this.data?.ulb?.populationBucket != ulb?.populationBucket) {
+    //   Swal.fire({
+    //     title: 'Are you sure?',
+    //     text: `${ulb?.name} does not fall under ${this.data?.bucketShortName} if you still want to compare, please click on apply button.`,
+    //     icon: 'warning',
+    //     showCancelButton: true,
+    //     confirmButtonColor: '#3085d6',
+    //     cancelButtonColor: '#d33',
+    //     confirmButtonText: 'Apply',
+    //   }).then((result) => {
+    //     if (result.isConfirmed) {
+    //       this.ulbs.push(ulb);
+    //     }
+    //   });
+    // } else if (
+    //   this.data?.ulb?.name === ulb?.name ||
+    //   this.ulbs?.some((element: any) => element?.name === ulb?.name)
+    // ) {
+    //   Swal.fire({
+    //     title: 'Oops!',
+    //     text: `${ulb?.name ?? 'Serached ULB'} already exists.`,
+    //   });
+    // } else {
+    //   this.ulbs.push(ulb);
+    // }
+    // this.searchField.setValue('');
+    // this.searchResults = [];
   }
 
 }
