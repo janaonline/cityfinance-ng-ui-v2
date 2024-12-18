68b63f4bbfc8e80ce675367af9bf4b842ebee90d
jeevad <jeevananthamcse@gmail.com>
2024-12-18 16:33:04 +0530

    Dynamic GA integrated

---
+++ src/app/core/services/google-analytics.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+declare let gtag: (command: string, eventName: string | Date, params?: Record<string, any>) => void;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GoogleAnalyticsService {
+
+  constructor(private router: Router) { }
+
+  init() {
+    this.appendScript();
+    this.onPageView();
+  }
+
+  appendScript() {
+    const script = document.createElement('script');
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${environment.googleAnalyticsId}`;
+    script.async = true;
+    document.getElementsByTagName('head')[0].appendChild(script);
+    const gtagEl = document.createElement('script');
+    const gtagBody = document.createTextNode(`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+    `);
+    gtagEl.appendChild(gtagBody);
+    document.body.appendChild(gtagEl);
+  }
+
+  onPageView() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      const url = event.urlAfterRedirects;
+      gtag('js', new Date());
+      gtag('config', environment.googleAnalyticsId, { page_path: url });
+    });
+  }
+
+  trackEvent(action: string, params: any) {
+    gtag('event', action, params);
+  }
+
+  // trackEvent1(eventName: string, params: any) {
+  //   if ((window as any).gtag) {
+  //     (window as any).gtag('event', eventName, params);
+  //   }
+  // }
+}
