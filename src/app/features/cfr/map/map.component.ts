d3ef42ac9210419dc552885b98d4f69e4d3b1f80
Navinder Singh <navinder.singh@janaagraha.org>
2025-04-24 12:10:01 +0530

    Add India & State map.

--- src/app/features/cfr/map/map.component.ts
+++ src/app/features/cfr/map/map.component.ts
@@ -1,131 +1,123 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnDestroy, OnInit } from '@angular/core';
 import * as L from 'leaflet';
-import { IMapCreationConfig, IStateLayerStyle } from '../../../core/util/map/models/mapCreationConfig';
-import { MapUtil } from '../../../core/util/map/mapUtil';
-import { GeographicalService } from '../../../core/services/geographical/geographical.service';
+import { allUlbsData } from '../../../../assets/jsonFile/ulbsListLocalStorage';
+import { UserUtility } from '../../../core/util/user/user';
+import { GeoJsonFeature, MapConfig, StateGeoJson, ULBDataPoint } from './interfaces';
+import { MapService } from './map.service';
+
 @Component({
   selector: 'app-map',
   standalone: true,
   imports: [],
-  templateUrl: './map.component.html',
-  styleUrl: './map.component.scss'
+  providers: [UserUtility],
+  template: ` <button class="btn btn-outline-cfPrimary" (click)="resetMap()">Reset</button>
+    <div id="map" [class.state-highlight]="stateCode"></div>`,
+  styles: [
+    `
+      * {
+        font-family: var(--ff-base);
+      }
+    `,
+    `
+      #map {
+        width: 100%;
+        height: 100vh;
+      }
+    `,
+    `
+      ::ng-deep .state-highlight path.leaflet-interactive {
+        filter: drop-shadow(0px 1px 0px #1e3571) drop-shadow(0px 2px 0px #1e3571)
+          drop-shadow(0px 3px 0px #1e3571) drop-shadow(0px 4px 0px #1e3571)
+          drop-shadow(0px 5px 0px #1e3571) drop-shadow(0px 6px 0px #1e3571)
+          drop-shadow(0px 7px 0px #1e3571);
+        stroke-width: 0 !important;
+      }
+    `,
+  ],
 })
-export class MapComponent implements AfterViewInit {
-  private map!: L.Map;
-
-  // Center coordinates of India
-  private indiaCenter: L.LatLngExpression = [20.5937, 78.9629];
-  StatesJSONForMapCreation: any;
-  defaultStateLayerStyle: IStateLayerStyle = {
-    fillColor: '#efefef',
-    weight: 1,
-    opacity: 1,
-    color: '#403f3f',
-    fillOpacity: 1,
+export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
+  @Input() stateCode: string = '';
+  private readonly DEFAULT_ZOOM_LEVEL = 4.4;
+  private ulbsList: ULBDataPoint[] = [];
+  private stateLayer: L.GeoJSON | null = null; // To hold current state layer.
+  private mapConfig: MapConfig = {
+    initialView: [19.59, 78.96],
+    initialZoom: this.DEFAULT_ZOOM_LEVEL,
+    minZoom: this.DEFAULT_ZOOM_LEVEL,
+    maxZoom: this.DEFAULT_ZOOM_LEVEL + 2,
   };
 
-  constructor(protected _geoService: GeographicalService,) {
-    this.createNationalMapJson();
+  constructor(private mapService: MapService) {}
+
+  ngOnInit(): void {
+    if (this.stateCode) this.loadMapData();
   }
 
   // Initialize the map after view is ready
   ngAfterViewInit(): void {
-    console.log('this.StatesJSONForMapCreation', this.StatesJSONForMapCreation);
-    this.initMap();
-  }
+    this.mapService.initMap('map', this.mapConfig);
+    this.loadMapData();
 
-  createNationalMapJson() {
-    const promise = this._geoService.loadConvertedIndiaGeoData().subscribe((data) => {
-      this.StatesJSONForMapCreation = data;
-      // this.initMap();
-      L.geoJSON(this.StatesJSONForMapCreation, {
-        style: this.defaultStateLayerStyle,
-      }).addTo(this.map)
-
+    // If any state is clicked - initiate state map.
+    this.mapService.stateCodeClicked$.subscribe((code) => {
+      if (!this.stateCode) {
+        this.stateCode = code;
+        this.loadMapData();
+      }
     });
-    // promise.then((data) => (this.StatesJSONForMapCreation = data));
-    // return Promise.all([promise]);
   }
 
-  private initMap(): void {
-    const options = {
-      "scrollWheelZoom": false,
-      "fadeAnimation": true,
-      "minZoom": 4.2,
-      "maxZoom": 4.2,
-      "zoomControl": false,
-      "keyboard": false,
-      "attributionControl": false,
-      "doubleClickZoom": false,
-      "dragging": false,
-      "tap": false,
-      "zoom": 4.2
-    };
-    // Initialize the map with the center on India
-    // this.map = L.map('map', {
-    //   center: this.indiaCenter,
-    //   zoom: 5
-    // });
-    this.map = L.map('map', options).setView([20.59, 78.96], 0.1);
+  // Get India/ state json - Structure.
+  private loadMapData(): void {
+    // Clear the previous state layer if it exists
+    if (this.stateLayer) {
+      this.mapService.map?.removeLayer(this.stateLayer);
+      this.stateLayer = null;
+    }
 
-    // const stateLayers = MapUtil.applyStyleOnStates(
-    //   configuration.geoData,
-    //   configuration.layerOptions,
-    // );
+    this.mapService.loadAndAddStates().subscribe({
+      next: (data: StateGeoJson) => {
+        // Fetch the JSON data for the specified state if 'stateCode' is provided; otherwise, fetch the json for all of India.
+        const geoJson = this.stateCode
+          ? data['features'].filter(
+              (stateObj: GeoJsonFeature) => this.stateCode === stateObj['properties']['ST_CODE'],
+            )
+          : data['features'];
 
-    // L.geoJSON(this.StatesJSONForMapCreation, {
-    //   style: this.defaultStateLayerStyle,
-    // }).addTo(this.map)
+        const stateGeoJsonData: StateGeoJson = {
+          type: 'FeatureCollection',
+          features: geoJson,
+        };
 
-    this.map.touchZoom.disable();
-    this.map.doubleClickZoom.disable();
-    this.map.scrollWheelZoom.disable();
-    this.map.boxZoom.disable();
-    this.map.keyboard.disable();
-    this.map.dragging.disable();
+        this.stateLayer = this.mapService.addGeoJsonLayer(stateGeoJsonData, this.stateCode);
 
-    // Add OpenStreetMap tile layer
-    // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //   maxZoom: 18,
-    //   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    // }).addTo(this.map);
-
-    // // Optional: Add a marker for New Delhi
-    const marker = L.marker([28.6139, 77.2090], {
-      icon: new L.Icon({
-        // iconUrl: 'assets/images/maps/simple_blue_dot.png',
-        // iconSize: [10, 10],
-        // iconAnchor: [6, 6],
-        iconUrl: 'assets/images/maps/map-marker.png',
-        iconSize: [20, 20],
-        iconAnchor: [10, 10],
-      }), title: 'delhi'
-    }).addTo(this.map);
-    marker.bindPopup("<b>New Delhi</b><br>Capital of India"); //.openPopup();
+        // Send geoJson of state if 'stateCode' is provided; otherwise, send the json for all of India.
+        if (this.stateCode && geoJson.length) {
+          this.mapService.flyToStateBounds(this.stateLayer, [20, 20], 2, 0.5);
+          this.getCityCordinates();
+          this.mapService.addCityMarkersToMap(this.stateCode, this.ulbsList);
+        } else {
+          this.mapService.map?.setView(this.mapConfig.initialView, this.mapConfig.initialZoom); // show the entire country
+          this.mapService.clearCityMarkers(); // clear markers when showing the whole country
+        }
+      },
+      error: (error) => console.error('Error loading map data in component:', error),
+    });
   }
 
-  // public static createDefaultNationalMap(configuration: IMapCreationConfig) {
-  //   console.log(configuration);
-  //   const options = configuration.options
-  //     ? { ...MapUtil.defaultMapConfiguration, ...configuration.options }
-  //     : MapUtil.defaultMapConfiguration;
-  //   let map = L.map(configuration.containerId, options).setView([20.59, 78.96], 0.1);
+  // Return ULB details obj - with lat & lng of ULBs for blue dot.
+  private getCityCordinates(): void {
+    this.ulbsList = this.stateCode ? allUlbsData[`${this.stateCode}`]['ulbs'] : [];
+  }
 
-  //   const stateLayers = MapUtil.applyStyleOnStates(
-  //     configuration.geoData,
-  //     configuration.layerOptions,
-  //   ).addTo(map);
+  public resetMap(): void {
+    this.stateCode = '';
+    this.loadMapData();
+    this.mapService.map?.setView(this.mapConfig.initialView, this.mapConfig.initialZoom);
+    this.mapService.clearCityMarkers();
+  }
 
-  //   map = MapUtil.centerMap(map, stateLayers);
-  //   map.touchZoom.disable();
-  //   map.doubleClickZoom.disable();
-  //   map.scrollWheelZoom.disable();
-  //   map.boxZoom.disable();
-  //   map.keyboard.disable();
-  //   map.dragging.disable();
-
-  //   return { map, stateLayers };
-  //   //store map instance for feature reference to destroy.
-  // }
-
+  ngOnDestroy(): void {
+    this.mapService.destroyMap();
+  }
 }
