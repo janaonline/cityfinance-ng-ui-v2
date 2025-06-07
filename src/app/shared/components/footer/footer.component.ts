b69f35b7cd1a4620d5b7c78811c04bbe7f2f00db
Navinder Singh <navinder.singh@janaagraha.org>
2025-06-07 21:34:06 +0530

    Style footer.

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,17 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { NavigationEnd, Router } from '@angular/router';
 import { CommonService } from '../../../core/services/common.service';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
 export class FooterComponent implements OnInit {
   totalUsersVisit: number | undefined;
-
   footerLinks = [
     { href: '/home', title: 'Home' },
     { href: '/dashboard/national/61e150439ed0e8575c881028', title: 'Financial' },
@@ -19,6 +17,29 @@ export class FooterComponent implements OnInit {
     { href: '/dashboard/slb', title: 'Service Level Benchmarks Performance' },
     { href: '/resources-dashboard/learning-center/toolkits', title: 'Resources' },
   ];
+  socialMediaInfo = [
+    {
+      link: 'https://www.facebook.com/mohua.india',
+      imgSrc: './assets/images/social/fb.svg',
+      key: 'facebook-mohua',
+    },
+    {
+      link: 'https://twitter.com/MoHUA_India',
+      imgSrc: './assets/images/social/twitter.svg',
+      key: 'twitter-mohua',
+    },
+    {
+      link: 'https://www.linkedin.com/company/mohua/',
+      imgSrc: './assets/images/social/linkdin.svg',
+      key: 'linkedin-mohua',
+    },
+  ];
+  address = ` Director, AMRUT <br />
+    Ministry of Housing and Urban Affairs <br />
+    210 C, Nirman Bhawan, Maulana Azad Road <br />
+    New Delhi-110011`;
+  mailId = 'mailto:contact@cityfinance.in';
+  mailLabel = 'contact@cityfinance.in';
 
   constructor(
     private _commonService: CommonService,
@@ -32,20 +53,12 @@ export class FooterComponent implements OnInit {
   }
 
   private fetchUserVisitCount() {
-    this._commonService.getWebsiteVisitCount().subscribe((res: any) => {
+    this._commonService.getWebsiteVisitCount().subscribe((res: number) => {
       return (this.totalUsersVisit = res);
     });
   }
-  routerNav(navlink: any) {
-    console.log(navlink);
-  }
-  address = ` Director, AMRUT <br />
-    Ministry of Housing and Urban Affairs <br />
-    210 C, Nirman Bhawan, Maulana Azad Road <br />
-    New Delhi-110011`;
-  mailId = 'mailto:contact@cityfinance.in';
-  mailLabel = 'contact@cityfinance.in';
-  getPageDetails() {
+
+  private getPageDetails() {
     this.router.events.subscribe((event) => {
       let urlArray;
       if (event instanceof NavigationEnd) {
