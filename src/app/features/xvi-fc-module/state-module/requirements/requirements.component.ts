7690d173639d1680b8dd1e9ad249a3fe5b33588c
Navinder Singh <navinder.singh@janaagraha.org>
2026-03-15 22:37:23 +0530

    feat(styles): refactor SCSS structure and update theme references across components

--- src/app/features/xvi-fc-module/state-module/requirements/requirements.component.ts
+++ src/app/features/xvi-fc-module/state-module/requirements/requirements.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { AngularMultiSelectModule } from 'angular2-multiselect-dropdown';
+import { UtilityService } from '../../../../core/services/utility.service';
 
 @Component({
   selector: 'app-requirements',
-  imports: [],
+  imports: [AngularMultiSelectModule],
   templateUrl: './requirements.component.html',
-  styleUrl: './requirements.component.scss'
+  styleUrl: './requirements.component.scss',
 })
 export class RequirementsComponent {
+  utilityService = inject(UtilityService);
 
+  conditionsToBeMet = signal([
+    {
+      label: 'SFC Status',
+      desc: 'State Finance Commission constituted and notified - to be confirmed',
+    },
+    {
+      label: 'Elected Body Status',
+      desc: 'Confirmation that ULBs have elected councils in place - to be uploaded',
+    },
+    {
+      label: 'Devolution Formula',
+      desc: 'Upload the Excel file showing grant amounts and devolution formula for each ULB',
+    },
+  ]);
+
+  upload(label: string) {
+    this.utilityService.triggerSnackbar(`${label} uploaded successfully!`);
+  }
 }
