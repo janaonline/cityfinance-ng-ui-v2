627ce90caee352624d4ecea2303ad7bfd50c070b
Navinder Singh <navinder.singh@janaagraha.org>
2026-07-14 16:06:48 +0530

    feat(fc-unspent-declaration): #DJ2-463 | add component and routing for unspent declaration

---
+++ src/app/features/xvi-fc-module/state-module/fc-unspent-declaration/fc-unspent-declaration.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fc-unspent-declaration',
+  imports: [],
+  templateUrl: './fc-unspent-declaration.component.html',
+  styleUrl: './fc-unspent-declaration.component.scss'
+})
+export class FcUnspentDeclarationComponent {
+
+}
